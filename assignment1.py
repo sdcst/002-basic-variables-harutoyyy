5ad73e87366b4304b4d82f80c88161d4bbe02bb7
harutoyyy <haluomu0530@gmail.com>
2024-04-04 13:08:23 -0700

    done

--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,12 @@
 #! python3
 
 # SD Computing Studies Assignment
+#Create 2 variables named x and y.  Assign a value of 10 to x and a value of 2.4 to y\
+#Output is not necessary.  The contents of the variables will be checked from within the program.\
+
+x = 10
+y = 2.4
+print(x)
+print(y)
+
+
